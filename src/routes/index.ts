ec787e232163a4c156a1016e3944b7543332f34f
Jan Szumotalski <jszumotalski@gmail.com>
2025-05-31 15:34:04 +0200

    feat: Added a healthcheck to backend

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,12 +5,21 @@ import { adminRouter } from "./api/adminRouter";
 import { appRouter } from "./api/appRouter";
 import { authRouter } from "./auth/index";
 import capability, { Features } from "@/helpers/capability";
+import mongoose from "mongoose";
 
 const router = Router();
 
 router.use('/app', appRouter)
 router.use('/admin', adminRouter)
 router.use('/auth', authRouter)
+
+router.get("/healthcheck", async (req, res) => {
+    res.status(200).send({
+        uptime: process.uptime(),
+        date: new Date(),
+        db: mongoose.connection.readyState
+    })
+})
 
 router.post("/notif", islogged, capability.mw(Features.Notif), async (req, res) => {
     var obj = {user: req.user._id, ...req.body}
