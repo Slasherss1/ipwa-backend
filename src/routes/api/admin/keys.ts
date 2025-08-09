aa466423d351fd1320989da29021a5d0aab52971
Jan Szumotalski <jszumotalski@gmail.com>
2025-08-09 21:01:30 +0200

    fix: added user data to keys

--- src/routes/api/admin/keys.ts
+++ src/routes/api/admin/keys.ts
@@ -11,7 +11,10 @@ keysRouter.use(capability.mw(Features.Key))
 keysRouter.use(adminPerm(Perms.Key))
 
 keysRouter.get("/", async (req, res) => {
-    var keys = await Key.find({}, undefined, {sort: {borrow: -1}}).populate<Pick<IUser, "uname" | "room"> & {_id: string}>({path: "whom", select: { _id: 1, uname: 1, room: 1}})
+    var keys = await Key.find({}, undefined, { sort: { borrow: -1 } })
+        .populate
+        <Pick<IUser, "uname" | "room" | "fname" | "surname"> & { _id: string }>
+        ({ path: "whom", select: { _id: 1, uname: 1, room: 1, fname: 1, surname: 1 } })
     res.send(keys)
 })
 
@@ -25,14 +28,14 @@ keysRouter.post("/", async (req, res) => {
         whom: user._id
     })
     if (await newKey.save()) {
-        res.status(201).send({status: 201})
+        res.status(201).send({ status: 201 })
     } else {
         res.sendStatus(500)
     }
 })
 
 keysRouter.get("/available", async (req, res) => {
-    var taken = await Key.find({tb: {$exists: false}}, {}, {sort: {borrow: -1}})
+    var taken = await Key.find({ tb: { $exists: false } }, {}, { sort: { borrow: -1 } })
     var occ = Array.from(new Set(taken.map((v) => v.room)))
     var all = Array.from(new Set(usettings.value.keyrooms))
     var free = all.filter(x => !occ.includes(x))
@@ -41,7 +44,7 @@ keysRouter.get("/available", async (req, res) => {
 
 keysRouter.put("/:id", async (req, res) => {
     if (await Key.findByIdAndUpdate(req.params.id, req.body)) {
-        res.send({status: 200})
+        res.send({ status: 200 })
     } else {
         res.sendStatus(500)
     }
